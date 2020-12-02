163022df49ce18f02c8df21bb49b1e02f5cdec9a
Stephen Li <me@ziyunli.dev>
2020-12-01 23:34:23 -0500

    day01 (rust)

---
+++ rust/src/bin/day01.rs
@@ -0,0 +1,23 @@
+extern crate aoc2020;
+use aoc2020::common::read_lines;
+
+use std::collections::HashSet;
+
+fn main() {
+    // File hosts must exist in current path before this produces output
+    if let Ok(lines) = read_lines("../input/01.txt") {
+        let mut found: HashSet<i32> = HashSet::new();
+        // Consumes the iterator, returns an (Optional) String
+        for line in lines {
+            if let Ok(ip) = line {
+                let n = ip.parse::<i32>().unwrap();
+                let m = 2020 - n;
+                if found.contains(&m) {
+                    println!("{} * {} = {}", n, m, n * m);
+                    return
+                }
+                found.insert(n);
+            }
+        }
+    }
+}
---
+++ rust/src/bin/day01ex.rs
@@ -0,0 +1,29 @@
+extern crate aoc2020;
+
+use aoc2020::common::read_lines;
+
+use std::collections::HashSet;
+
+fn main() {
+    if let Ok(lines) = read_lines("../input/01.txt") {
+        let nums = lines.map(|line|
+            line.unwrap().parse::<i32>().unwrap()
+        ).collect::<Vec<_>>();
+        let length = nums.len();
+
+        for i in 0..length-1 {
+            let mut found: HashSet<i32> = HashSet::new();
+            let a = nums[i];
+            let rem = 2020 - a;
+            for j in i + 1..length {
+                let b = nums[j];
+                let c = rem - b;
+                if found.contains(&c) {
+                    println!("{} * {} * {} = {}", a, b, c, a * b * c);
+                    return;
+                }
+                found.insert(b);
+            }
+        }
+    }
+}
